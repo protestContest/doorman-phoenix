2935a9456e26fc1c6652d8ad9a33ce966d6a5a85
Zack Michener <z@zjm.me>
2019-07-29 11:39:18 -0700

    force ssl in production

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :doorman, DoormanWeb.Endpoint,
-  url: [host: System.get_env("SITE_DOMAIN"), port: 80],
+  url: [scheme: "https", host: System.get_env("SITE_DOMAIN"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
